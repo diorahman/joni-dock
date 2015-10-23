2a255052bc994b0de6493d08b700241897b37057
Dhi Aurrahman <diorahman@rockybars.com>
2015-10-23 15:28:52 +0700

    Add a simple static page

--- server.js
+++ server.js
@@ -1,65 +1,26 @@
 var Ws = require('ws');
-var Fs = require('fs');
-var Docker = require('dockerode');
+var St = require('st');
+var Http = require('http');
+var HttpHashRouter = require('http-hash-router');
+var dock = require('./lib');
 
-var Server = Ws.Server;
+var router = HttpHashRouter();
+router.set('*', St(__dirname + '/static'));
+
 var port = process.env.PORT || 8000;
-var server = new Server({port: port});
+var server = Http.createServer(function handler(req, res) {
+    router(req, res, {}, onError);
 
-// FIXME: stat the docket socket first!
-var docker = new Docker();
-
-server.on('connection', function (ws) {
-    ws.send('please wait...');
-    docker.createContainer(require('./opt'), function(err, container) {
-
-        var attachOptions = {
-            stream: true,
-            stdin: true,
-            stdout: true,
-            stderr: true
-        };
-
-        var line = '';
-
-        container.attach(attachOptions, function(err, stream) {
-
-            ws.send('attached.');
-
-            ws.on('message', function(message) {
-                stream.write(message);
-                stream.write(String.fromCharCode(13));
-            });
-
-            stream.on('data', function(chunk) {
-                var str = chunk.toString();
-                for (var i = 0; i < str.length; i++) {
-                    if (str.charCodeAt(i) === 13) {
-                        ws.send(line);
-                        line = '';
-                    }
-                    line += str.charAt(i);
-                }
-            });
-
-            container.start(function(err, data) {
-
-                ws.send('started.');
-
-                container.wait(function(err) {
-
-                    // FIXME: cleanup
-
-                });
-            });
-        });
-    });
-
-    ws.on('close', function() {
-        // FIXME: stop and remove container
-    });
+    function onError(err) {
+        if (err) {
+            res.statusCode = err.statusCode || 500;
+            res.end(err.message);
+        }
+    }
 });
+var websocketServer = new Ws.Server({server: server});
+websocketServer.on('connection', function(ws) {
+    dock(ws, require('./opts'));
+});
+server.listen(port);
 
-console.log('~> running at', port);
-console.log('To connect you can use wscat (npm install -g wscat)');
-console.log('$ wscat -c ws://localhost:' + port);
